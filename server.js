89e1552ddbfef0258e884199d62c32949b46a5db
Ting Deng <tingdeng@Tings-MacBook-Pro.local>
2022-01-06 00:37:20 -0800

    fix the bug of res.sendFile

--- server.js
+++ server.js
@@ -25,5 +25,9 @@ app.post("/api/pokemons", (req, res) => {
   res.send(data)
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/build/index.html');
+});
+
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
